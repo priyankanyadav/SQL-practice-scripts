704f6c2998205c2963bfb4703210a97341244442
Priyanka Yadav <priyankanyadav1144@gmail.com>
2025-08-12 11:38:28 -0400

    Create SelectQuery.sql

---
+++ SelectQuery.sql
@@ -0,0 +1,7 @@
+--Select Query we can select all of the records from database.
+
+select * from Students
+select * from Courses
+select * from Instructors
+select * from CourseAssignments
+select * from Enrollments
